0161f66b4ec99266e730931f43dbedf2deb2dad4
Ameenah Burhan <ameenah.burhan@gmail.com>
2023-03-09 12:29:04 -0500

    VPA Recommendation Tutorial - Updates to image location (#514)
    * initial commit
    
    * initial commit
    
    * updated code
    
    * added files
    
    * update commands
    
    * updated deploy instructions
    
    * work:
    
    * Initial commit
    
    * updated readme
    
    * Update .gitignore
    
    * Update .gitignore
    
    * removed WIP folder
    
    * removed folder
    
    * removed tags
    
    * removed broken tag
    
    * work
    
    * initial commit
    
    * initial commit
    
    * inital commit
    
    * update
    
    * fixed cloud scheduler permission error
    
    * update
    
    * updated hpa exporter image name
    
    * removed unnecessary files
    
    * .gitignore
    
    * update gitignore
    
    * workflow files
    
    * updated ci files
    
    * update
    
    * initial commit
    
    * remove gke-stateful-kafka folder
    
    * removed kafka folder
    
    * Update .gitignore
    
    * Fix VPA recommendations CI workflow
    
    * Fix typo in metrics-exporter Cloud Build config
    
    * Delete google_cloudbuild.yaml
    
    * Fix typo in hpa-metrics-exporter Cloud Build config
    
    * Fix typos in TF config
    
    * update image location
    
    * remove metric export file
    
    ---------
    
    Co-authored-by: Olivier Bourgeois <3271352+bourgeoisor@users.noreply.github.com>

--- gke-vpa-recommendations/scripts/deploy_pipeline.sh
+++ gke-vpa-recommendations/scripts/deploy_pipeline.sh
@@ -1,27 +1,20 @@
 
-echo "Create a service account to run the pipeline"
+#1. Create a service account to run the pipeline
 gcloud iam service-accounts create mql-export-metrics \
 --display-name "MQL export metrics SA" \
 --description "Used for the function that export monitoring metrics"
 
-echo "Assigning IAM roles to the service account..."
+#1. Assigning IAM roles to the service account
 gcloud projects add-iam-policy-binding  $PROJECT_ID --member="serviceAccount:mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/monitoring.viewer"
 gcloud projects add-iam-policy-binding  $PROJECT_ID --member="serviceAccount:mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.dataEditor"
 gcloud projects add-iam-policy-binding  $PROJECT_ID --member="serviceAccount:mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.dataOwner"
 gcloud projects add-iam-policy-binding  $PROJECT_ID --member="serviceAccount:mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.jobUser"
 gcloud projects add-iam-policy-binding  $PROJECT_ID --member="serviceAccount:mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/run.invoker"
 
-echo "Creating the Pub/Sub topic..."
-gcloud pubsub topics create metric_export
 
-
-echo "Build image and store the image in Artifact registry..."
-gcloud builds submit metrics-exporter --config=metrics-exporter/cloudbuild.yaml  --substitutions=_REGION=$REGION
-
-
-echo "Deploy the Cloud Run Job.."
+#1. Deploy the Cloud Run Job
 gcloud beta run jobs deploy metric-exporter \
-    --image=$REGION-docker.pkg.dev/$PROJECT_ID/main/metric-exporter \
+    --image=us-docker.pkg.dev/google-samples/containers/gke/metrics-exporter:latest \
     --set-env-vars=PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python \
     --set-env-vars=PROJECT_ID=$PROJECT_ID \
     --execute-now \
@@ -31,15 +24,10 @@ gcloud beta run jobs deploy metric-exporter \
     --service-account=mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com \
     --region=$REGION
 
-echo "Enable the Cloud Scheduler api.."
-gcloud services enable cloudscheduler.googleapis.com
-
-
+#1. Create a schedule to run once a day
 gcloud scheduler jobs create http metric-exporter \
   --location $REGION \
   --schedule="0 23 * * *" \
   --uri="https://$REGION-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/$PROJECT_ID/jobs/metric-exporter:run" \
   --http-method POST \
   --oauth-service-account-email "mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com"
-
-echo "Deployment complete"
