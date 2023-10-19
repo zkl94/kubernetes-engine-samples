1cf87d1158c34ce0ddff4a56007b6241b0937aae
ganochenkodg <52278182+ganochenkodg@users.noreply.github.com>
2023-10-19 19:28:42 +0200

    Spotahome Redis operator - fixes (#952)
    * resolve comments
    
    * add ann and restartPolicy to the client pod
    
    * fix tf versions
    
    * update databases-ci.yaml
    
    * add geaders

--- databases/redis-spotahome/terraform/gke-autopilot/versions.tf
+++ databases/redis-spotahome/terraform/gke-autopilot/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
   }
   required_version = ">= 0.13"
