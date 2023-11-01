925847cef6a4b4d1ebd08e5b581ce54109281bdd
Olivier Bourgeois <3271352+bourgeoisor@users.noreply.github.com>
2023-11-01 16:15:17 -0400

    Add missing region tags to files used in tutorials (#1001)

--- quickstarts/languages/dotnet/Program.cs
+++ quickstarts/languages/dotnet/Program.cs
@@ -1,17 +1,18 @@
-﻿// Copyright (c) 2019 Google LLC.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// use this file except in compliance with the License. You may obtain a copy of
-// the License at
-// 
-// http://www.apache.org/licenses/LICENSE-2.0
-// 
+﻿// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations under
-// the License.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+// [START gke_quickstarts_languages_dotnet_program]
 var builder = WebApplication.CreateBuilder(args);
 
 // Google Cloud Run sets the PORT environment variable to tell this
@@ -25,3 +26,4 @@ var app = builder.Build();
 app.MapGet("/", () => $"Hello {target}!");
 
 app.Run(url);
+// [END gke_quickstarts_languages_dotnet_program]
