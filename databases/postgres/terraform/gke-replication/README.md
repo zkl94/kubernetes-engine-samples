b03ff7a78cbde0de6e3067f583e3b4e8cd6430ad
ganochenkodg <52278182+ganochenkodg@users.noreply.github.com>
2023-11-13 21:56:59 +0100

    feat: samples for upcoming Zalando Postgres DR tutorial (#998)
    * first commit
    
    * tf quickfix
    
    * finish with replication
    
    * add s3 backups
    
    * add CI
    
    * add headers
    
    * pin TF versions
    
    * update provider version
    
    * update tf version requirements
    
    * disable deletion protection

---
+++ databases/postgres/terraform/gke-replication/README.md
@@ -0,0 +1,26 @@
+# Terraform to provision GKE Standard
+
+## Prerequisites and Assumptions
+* Done initialization of the project and gcloud CLI following the instructions in `{ROOT}/README.md`
+* VPC network, refer to `gke` folder for the details
+
+## Usage
+```
+export GOOGLE_OAUTH_ACCESS_TOKEN=$(gcloud auth print-access-token)
+export PROJECT_ID="your project"
+export REGION="us-central1"
+export KUBERNETES_CLUSTER_PREFIX="postgres"
+
+terraform init
+terraform plan -var project_id=$PROJECT_ID -var region=${REGION} -var cluster_prefix=${KUBERNETES_CLUSTER_PREFIX}
+terraform apply -var project_id=$PROJECT_ID -var region=${REGION} -var cluster_prefix=${KUBERNETES_CLUSTER_PREFIX}
+```
+## Clean up
+**NOTE:** Be very careful when destroying any resource, not recommended for production!
+```
+# Destroy everything
+terraform destroy \
+-var project_id=$PROJECT_ID \
+-var region=${REGION} \
+-var cluster_prefix=${KUBERNETES_CLUSTER_PREFIX}
+
---
+++ databases/postgres/terraform/modules/cluster-replication/main.tf
@@ -0,0 +1,83 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gke_postgres_standard_private_regional_cluster]
+module "postgres_cluster" {
+  source                   = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  version                  = "~> 29.0"
+  project_id               = var.project_id
+  name                     = "${var.cluster_prefix}-cluster"
+  regional                 = true
+  region                   = var.region
+  network                  = var.network
+  subnetwork               = var.subnetwork
+  ip_range_pods            = "k8s-pod-range"
+  ip_range_services        = "k8s-service-range"
+  create_service_account   = true
+  enable_private_endpoint  = false
+  enable_private_nodes     = true
+  master_ipv4_cidr_block   = var.master_ipv4_cidr_block
+  network_policy           = true
+  logging_enabled_components = ["SYSTEM_COMPONENTS","WORKLOADS"]
+  monitoring_enabled_components = ["SYSTEM_COMPONENTS"]
+  enable_cost_allocation = true
+  deletion_protection = false
+  initial_node_count = 1
+
+  cluster_dns_domain   = "${var.cluster_prefix}.local"
+  cluster_dns_provider = "CLOUD_DNS"
+  cluster_dns_scope    = "VPC_SCOPE"
+
+  cluster_resource_labels = {
+    name      = "${var.cluster_prefix}-cluster"
+    component = "${var.cluster_prefix}-operator"
+  }
+
+  monitoring_enable_managed_prometheus = true
+  gke_backup_agent_config = true
+ 
+  node_pools = [
+    {
+      name            = "pool-postgres"
+      disk_size_gb    = 20
+      disk_type       = "pd-standard"
+      autoscaling     = true
+      min_count       = 1
+      max_count       = 2
+      max_surge       = 1
+      max_unavailable = 0
+      machine_type    = "e2-standard-2"
+      auto_repair     = true
+    }
+  ]
+  node_pools_labels = {
+    all = {}
+    pool-postgres = {
+      "app.stateful/component" = "postgres-operator"
+    } 
+  }
+  node_pools_taints = {
+    all = []
+    pool-redis = [
+      {
+        key    = "app.stateful/component"
+        value  = "postgres-operator"
+        effect = "NO_SCHEDULE"
+      }
+    ]
+  }
+  gce_pd_csi_driver = true
+}
+# [END gke_postgres_standard_private_regional_cluster]
+
