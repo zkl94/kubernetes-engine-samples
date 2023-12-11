b07171ba37b149d934c8139bb34e927ff90349ea
BRV158 <141618301+BRV158@users.noreply.github.com>
2023-12-11 22:55:11 +0200

    feat: samples for upcoming CloudNativePG operator tutorial (#1004)
    * cloudnativepg related snippets added, license fix
    
    * terraform modules version fix
    
    * disable deletion protection
    
    * host range tuned
    
    * bracket cleanup
    
    * disable deletion protection for standard
    
    * validation workflow added
    
    * license header added
    
    * license year fix, readme added
    
    * README URL edited
    
    * missed license year fix
    
    * naming fix
    
    * prefix added
    
    * spaces off
    
    * regional tags aligned
    
    * another regional tag change
    
    * duplicated tag edited
    
    * vpc network fix

---
+++ databases/postgresql-cloudnativepg/README.md
@@ -0,0 +1,7 @@
+# Deploying a PostgreSQL cluster with CLOUDNATIVEPG operator on GKE
+
+[![Open in Cloud Shell](https://gstatic.com/cloudssh/images/open-btn.svg)](https://ssh.cloud.google.com/cloudshell/editor?cloudshell_git_repo=https://github.com/GoogleCloudPlatform/kubernetes-engine-samples&cloudshell_tutorial=cloudshell/tutorial.md&cloudshell_workspace=gke-postgresql-cloudnativepg)
+
+This example shows how to build and deploy a PostgreSQL cluster using [Kubernetes](https://kubernetes.io) and Day 1 & 2 operations such as Monitoring with [Cloud Monitoring](https://cloud.google.com/monitoring) and disaster recovery with [Backup for GKE](https://cloud.google.com/kubernetes-engine/docs/add-on/backup-for-gke/concepts/backup-for-gke).
+
+Follow this tutorial at [https://cloud.google.com/kubernetes-engine/docs/tutorials/stateful-workloads/cloudnativepg](https://cloud.google.com/kubernetes-engine/docs/tutorials/stateful-workloads/cloudnativepg)
---
+++ databases/postgresql-cloudnativepg/terraform/gke-standard/main.tf
@@ -0,0 +1,71 @@
+#Copyright 2023 Google LLC
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+# google_client_config and kubernetes provider must be explicitly specified like the following.
+data "google_client_config" "default" {}
+
+# create private subnet
+module "network" {
+  source         = "../modules/network"
+  project_id     = var.project_id
+  region         = var.region
+  cluster_prefix = var.cluster_prefix
+}
+
+# [START gke_databases_postgresql_cloudnativepg_standard_private_regional_cluster]
+module "postgresql_cluster" {
+  source                   = "../modules/cluster"
+  project_id               = var.project_id
+  region                   = var.region
+  cluster_prefix           = var.cluster_prefix
+  network                  = module.network.network_name
+  subnetwork               = module.network.subnet_name
+
+  node_pools = [
+    {
+      name            = "pool-postgresql"
+      disk_size_gb    = 50
+      disk_type       = "pd-standard"
+      autoscaling     = true
+      min_count       = 1
+      max_count       = 2
+      max_surge       = 1
+      max_unavailable = 0
+      machine_type    = "e2-standard-4"
+      auto_repair     = true
+      
+    }
+  ]
+  node_pools_labels = {
+    all = {}
+    pool-postgresql = {
+      "app.component" = "pg-cluster"
+    }
+  }
+  node_pools_taints = {
+    all = []
+    pool-postgresql = [
+      {
+        key    = "cnpg.io/cluster"
+        value  = "gke-pg-cluster"
+        effect = "NO_SCHEDULE"
+      }
+    ]
+  }
+}
+
+output "kubectl_connection_command" {
+  value       = "gcloud container clusters get-credentials ${var.cluster_prefix}-cluster --region ${var.region}"
+  description = "Connection command"
+}
+# [END gke_databases_postgresql_cloudnativepg_standard_private_regional_cluster]
