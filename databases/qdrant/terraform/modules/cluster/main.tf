e55ec22aa7075ac051a34d3bc24b8791090b2f4b
Ameenah Burhan <ameenah.burhan@gmail.com>
2024-03-19 10:35:10 -0400

    update cluster (#1211)
    * update cluster
    
    * updated HA object

--- databases/qdrant/terraform/modules/cluster/main.tf
+++ databases/qdrant/terraform/modules/cluster/main.tf
@@ -14,26 +14,27 @@
 
 # [START gke_qdrant_standard_private_regional_cluster]
 module "qdrant_cluster" {
-  source                   = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  version                  = "~> 29.0"
-  project_id               = var.project_id
-  name                     = "${var.cluster_prefix}-cluster"
-  regional                 = true
-  region                   = var.region
-  network                  = var.network
-  subnetwork               = var.subnetwork
-  ip_range_pods            = "k8s-pod-range"
-  ip_range_services        = "k8s-service-range"
-  create_service_account   = true
-  enable_private_endpoint  = false
-  enable_private_nodes     = true
-  master_ipv4_cidr_block   = "172.16.0.0/28"
-  network_policy           = true
-  logging_enabled_components = ["SYSTEM_COMPONENTS","WORKLOADS"]
+  source                        = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  version                       = "30.2.0"
+  project_id                    = var.project_id
+  name                          = "${var.cluster_prefix}-cluster"
+  regional                      = true
+  region                        = var.region
+  network                       = var.network
+  subnetwork                    = var.subnetwork
+  ip_range_pods                 = "k8s-pod-range"
+  ip_range_services             = "k8s-service-range"
+  create_service_account        = true
+  enable_private_endpoint       = false
+  enable_private_nodes          = true
+  master_ipv4_cidr_block        = "172.16.0.0/28"
+  network_policy                = true
+  logging_enabled_components    = ["SYSTEM_COMPONENTS", "WORKLOADS"]
   monitoring_enabled_components = ["SYSTEM_COMPONENTS"]
-  enable_cost_allocation = true
-  deletion_protection = false
-  initial_node_count = 1
+  enable_cost_allocation        = true
+  deletion_protection           = false
+  initial_node_count            = 1
+  kubernetes_version            = "1.28"
 
   cluster_resource_labels = {
     name      = "${var.cluster_prefix}-cluster"
@@ -41,8 +42,8 @@ module "qdrant_cluster" {
   }
 
   monitoring_enable_managed_prometheus = true
-  gke_backup_agent_config = true
- 
+  gke_backup_agent_config              = true
+
   node_pools        = var.node_pools
   node_pools_labels = var.node_pools_labels
   node_pools_taints = var.node_pools_taints
