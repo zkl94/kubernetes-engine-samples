98a56630ea0d9dee86a2092606791f223e67a803
ganochenkodg <52278182+ganochenkodg@users.noreply.github.com>
2024-08-15 18:23:25 +0200

    Weaviate guide - code and examples (#1365)
    * first input - tf and basic helm manifest
    
    * weaviate  values updated with authentification
    
    * comments cleared out
    
    * notebook added
    
    * notebook and jupiter edited
    
    * notebook update
    
    * updates
    
    * update jupyter yaml
    
    * updates
    
    * updates
    
    * add dashboard
    
    * manifests tagged
    
    * end endpoint envs
    
    * update tf, add dockerfiles
    
    * update the code
    
    * small update
    
    * update chatbot.py
    
    * docker updates
    
    * update notebook, remove jupyter
    
    * update notebook
    
    * add ilb
    
    * fix the header
    
    * add ci step
    
    * ci quickfix
    
    ---------
    
    Co-authored-by: BRV158 <roman_buta@epam.com>

---
+++ databases/weaviate/docker/embed-docs/embedding-job.py
@@ -0,0 +1,64 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import weaviate
+from weaviate.connect import ConnectionParams
+from langchain_weaviate.vectorstores import WeaviateVectorStore
+from google.cloud import storage
+import os
+# [START gke_databases_weaviate_docker_embed_docs_retrieval]
+bucketname = os.getenv("BUCKET_NAME")
+filename = os.getenv("FILE_NAME")
+
+storage_client = storage.Client()
+bucket = storage_client.bucket(bucketname)
+blob = bucket.blob(filename)
+blob.download_to_filename("/documents/" + filename)
+# [END gke_databases_weaviate_docker_embed_docs_retrieval]
+
+# [START gke_databases_weaviate_docker_embed_docs_split]
+loader = PyPDFLoader("/documents/" + filename)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+documents = loader.load_and_split(text_splitter)
+# [END gke_databases_weaviate_docker_embed_docs_split]
+
+# [START gke_databases_weaviate_docker_embed_docs_embed]
+embeddings = VertexAIEmbeddings("textembedding-gecko@001")
+# [END gke_databases_weaviate_docker_embed_docs_embed]
+
+# [START gke_databases_weaviate_docker_embed_docs_storage]
+auth_config = weaviate.auth.AuthApiKey(api_key=os.getenv("APIKEY"))
+client = weaviate.WeaviateClient(
+    connection_params=ConnectionParams.from_params(
+        http_host=os.getenv("WEAVIATE_ENDPOINT"),
+        http_port="80",
+        http_secure=False,
+        grpc_host=os.getenv("WEAVIATE_GRPC_ENDPOINT"),
+        grpc_port="50051",
+        grpc_secure=False,
+    ),
+    auth_client_secret=auth_config
+)
+client.connect()
+if not client.collections.exists("trainingdocs"):
+    collection = client.collections.create(name="trainingdocs")
+db = WeaviateVectorStore.from_documents(documents, embeddings, client=client, index_name="trainingdocs")
+# [END gke_databases_weaviate_docker_embed_docs_storage]
+
+print(filename + " was successfully embedded") 
+print(f"# of vectors = {len(documents)}")
+ 
---
+++ databases/weaviate/docker/embed-docs/endpoint.py
@@ -0,0 +1,88 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from flask import Flask, jsonify
+from flask import request
+import logging
+import sys,os, time
+from kubernetes import client, config, utils
+import kubernetes.client
+from kubernetes.client.rest import ApiException
+
+
+app = Flask(__name__)
+@app.route('/check')
+def message():
+    return jsonify({"Message": "Hi there"})
+
+
+@app.route('/', methods=['POST'])
+def bucket():
+    request_data = request.get_json()
+    print(request_data)
+    bckt = request_data['bucket']
+    f_name = request_data['name']
+    id = request_data['generation'] 
+    kube_create_job(bckt, f_name, id)
+    return "ok"
+
+# Set logging
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+# Setup K8 configs
+config.load_incluster_config()
+# [START gke_databases_weaviate_docker_embed_endpoint_job]
+def kube_create_job_object(name, container_image, bucket_name, f_name, namespace, container_name="jobcontainer", env_vars={}):
+
+    body = client.V1Job(api_version="batch/v1", kind="Job")
+    body.metadata = client.V1ObjectMeta(namespace=namespace, name=name)
+    body.status = client.V1JobStatus()
+    
+    template = client.V1PodTemplate()
+    template.template = client.V1PodTemplateSpec()
+    env_list = [
+        client.V1EnvVar(name="WEAVIATE_ENDPOINT", value=os.getenv("WEAVIATE_ENDPOINT")),
+        client.V1EnvVar(name="WEAVIATE_GRPC_ENDPOINT", value=os.getenv("WEAVIATE_GRPC_ENDPOINT")),
+        client.V1EnvVar(name="FILE_NAME", value=f_name), 
+        client.V1EnvVar(name="BUCKET_NAME", value=bucket_name),
+        client.V1EnvVar(name="APIKEY", value_from=client.V1EnvVarSource(secret_key_ref=client.V1SecretKeySelector(key="AUTHENTICATION_APIKEY_ALLOWED_KEYS", name="apikeys"))), 
+    ]
+    
+    container = client.V1Container(name=container_name, image=container_image, image_pull_policy='Always', env=env_list)
+    template.template.spec = client.V1PodSpec(containers=[container], restart_policy='Never', service_account='embed-docs-sa')
+
+    body.spec = client.V1JobSpec(backoff_limit=3, ttl_seconds_after_finished=60, template=template.template)
+    return body
+# [END gke_databases_weaviate_docker_embed_endpoint_job]
+def kube_test_credentials():
+    try: 
+        api_response = api_instance.get_api_resources()
+        logging.info(api_response)
+    except ApiException as e:
+        print("Exception when calling API: %s\n" % e)
+
+def kube_create_job(bckt, f_name, id):
+    container_image = os.getenv("JOB_IMAGE")
+    namespace = os.getenv("JOB_NAMESPACE")
+    name = "docs-embedder" + id
+    body = kube_create_job_object(name, container_image, bckt, f_name, namespace)
+    v1=client.BatchV1Api()
+    try: 
+        v1.create_namespaced_job(namespace, body, pretty=True)
+    except ApiException as e:
+        print("Exception when calling BatchV1Api->create_namespaced_job: %s\n" % e)
+    return
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5001, debug=True)
